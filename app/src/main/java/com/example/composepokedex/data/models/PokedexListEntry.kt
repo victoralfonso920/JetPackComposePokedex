8fe6d192908343586a0cc4abd4bdda7b7f96e805
Victor Hernandez <victoralfonso920@gmail.com>
2021-08-10 18:34:37 -0600

    add rows and viewmodel

---
+++ app/src/main/java/com/example/composepokedex/data/models/PokedexListEntry.kt
@@ -0,0 +1,11 @@
+package com.example.composepokedex.data.models
+
+// Created by Victor Hernandez on 10/8/21.
+// Proyect ComposePokedex
+//contact victoralfonso920@gmail.com
+
+data class PokedexListEntry(
+    val pokemonName:String,
+    val imageUrl:String,
+    val number:String
+)
--- app/src/main/java/com/example/composepokedex/pokemonlist/PokemonListScreen.kt
+++ app/src/main/java/com/example/composepokedex/pokemonlist/PokemonListScreen.kt
@@ -2,9 +2,12 @@ package com.example.composepokedex.pokemonlist
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -14,7 +17,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -23,8 +25,23 @@ import androidx.navigation.NavController
 import com.example.composepokedex.R
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltNavGraphViewModel
+import androidx.hilt.navigation.compose.hiltViewModel
+import coil.request.ImageRequest
+import com.example.composepokedex.data.models.PokedexListEntry
+import com.example.composepokedex.ui.theme.RobotoCondensed
+import com.google.accompanist.coil.CoilImage
 
 @Composable
 fun PokemonListScreen(
@@ -89,7 +106,7 @@ fun SearchBar(
                 .background(Color.White, CircleShape)
                 .padding(horizontal = 20.dp, vertical = 12.dp)
                 .onFocusChanged {
-                     isHintDisplayed = it.isFocused != true
+                    isHintDisplayed = it.isFocused != true
                 }
 
         )
@@ -102,6 +119,97 @@ fun SearchBar(
                 )
             }
 
+    }
+}
+
+
+@Composable
+fun PokedexEntry(
+    entry: PokedexListEntry,
+    navController:NavController,
+    modifier:Modifier,
+    viewModel:PokemonListViewModel = hiltViewModel()
+){
+    val defaultDominantColor = MaterialTheme.colors.surface
+    var dominantColor by remember {
+        mutableStateOf(defaultDominantColor)
+    }
+
+    Box(
+        contentAlignment = Center,
+        modifier = modifier
+            .shadow(5.dp, RoundedCornerShape(10.dp))
+            .clip(RoundedCornerShape(10.dp))
+            .aspectRatio(1f)
+            .background(
+                Brush.verticalGradient(
+                    listOf(
+                        dominantColor,
+                        defaultDominantColor
+                    )
+                )
+            )
+            .clickable {
+                navController.navigate(
+                    "pokemon_detail_screen/${dominantColor.toArgb()}/${entry.pokemonName}"
+                )
+            }
+    ) {
+        Column {
+            CoilImage(request = ImageRequest.Builder(LocalContext.current)
+                .data(entry.imageUrl)
+                .target {
+                    viewModel.calcDominantColor(it){ color ->
+                       dominantColor = color
+                    }
+                }
+                .build(),
+            contentDescription = entry.pokemonName,
+                fadeIn = true,
+                modifier = Modifier
+                    .size(120.dp)
+                    .align(CenterHorizontally)
+            ) {
+                CircularProgressIndicator(
+                    color = MaterialTheme.colors.primary,
+                    modifier = Modifier.scale(0.5f)
+                )
+            }
+            Text(
+                text = entry.pokemonName,
+                fontFamily = RobotoCondensed,
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
+
+@Composable
+fun PokedexRow(
+    rowIndex:Int,
+    entries:List<PokedexListEntry>,
+    navController: NavController
+){
+    Column {
+        Row {
+            PokedexEntry(
+                entry = entries[rowIndex * 2],
+                navController = navController,
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            if(entries.size >= rowIndex * 2 + 2){
+                PokedexEntry(
+                    entry = entries[rowIndex * 2 + 1],
+                    navController = navController,
+                    modifier = Modifier.weight(1f)
+                )
+            }else{
+                Spacer(modifier = Modifier.weight(1f))
+            }
+        }
     }
 }
 
